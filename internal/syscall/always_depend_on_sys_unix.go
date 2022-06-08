82137b87d9ab20036fee2a6c3a60be3496851f11
Red Daly <reddaly@x.team>
2022-06-07 19:56:39 -0700

    Fix problems with bazel build caused by missing golang.org/x/sys/unix dependency.
    The problem seems to be related to the appengine build tag in some files.

---
+++ internal/syscall/always_depend_on_sys_unix.go
@@ -0,0 +1,10 @@
+package syscall
+
+/*
+Bazel fails to add this dep, probably because the appengine build tag forces our
+build to not include to files that typically import this library.
+
+Even if it is not used, importing it ensures gazelle always adds the dependency,
+which hopefully will trigger no errors.
+*/
+import _ "golang.org/x/sys/unix"
